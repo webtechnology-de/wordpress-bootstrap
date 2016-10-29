4f7d806f12ec200ae4c44d2989bc3c64770ee99e
Marco Lewandowski <marco.lewandowski@googlemail.com>
2016-10-30 00:31:03 +0200

    Test changes

--- footer.php
+++ footer.php
@@ -1,41 +1,25 @@
-			<footer role="contentinfo">
-			
-				<div id="inner-footer" class="clearfix">
-		          <hr />
-		          <div id="widget-footer" class="clearfix row">
-		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
-		            <?php endif; ?>
-		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
-		            <?php endif; ?>
-		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
-		            <?php endif; ?>
-		          </div>
-					
-					<nav class="clearfix">
-						<?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
-					</nav>
-					
-					<p class="pull-right"><a href="http://320press.com" id="credit320" title="By the dudes of 320press">320press</a></p>
-			
-					<p class="attribution">&copy; <?php bloginfo('name'); ?></p>
-				
-				</div> <!-- end #inner-footer -->
-				
-			</footer> <!-- end footer -->
-		
-		</div> <!-- end #container -->
-				
-		<!--[if lt IE 7 ]>
-  			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-  			<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-		<![endif]-->
-		
-		<?php wp_footer(); // js scripts are inserted using this function ?>
+<script>
+	(function (i, s, o, g, r, a, m) {
+		i['GoogleAnalyticsObject'] = r;
+		i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+		a = s.createElement(o),
+			m = s.getElementsByTagName(o)[0];
+		a.async = 1;
+		a.src = g;
+		m.parentNode.insertBefore(a, m)
+	})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-		<!-- remove this for production -->
-
-		<script src="//localhost:35729/livereload.js"></script>
-
-	</body>
+	ga('create', 'UA-63644784-1', 'auto');
+	ga('send', 'pageview');
+</script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery-2.1.1.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/pace.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/classie.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/cbpAnimatedHeader.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/inspinia.js"></script>
+</body>
 
 </html>
--- header.php
+++ header.php
@@ -7,64 +7,129 @@
 <!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 	
 	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title><?php wp_title( '|', true, 'right' ); ?></title>	
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<!-- wordpress head functions -->
-		<?php wp_head(); ?>
-		<!-- end of wordpress head -->
-		<!-- IE8 fallback moved below head to work properly. Added respond as well. Tested to work. -->
-			<!-- media-queries.js (fallback) -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		<!-- Add Your favicon here -->
+		<!--<link rel="icon" href="img/favicon.ico">-->
+
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css" rel="stylesheet">
+
+		<link href="<?php bloginfo('template_url'); ?>/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
-			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>			
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
-		<!-- html5.js -->
-		<!--[if lt IE 9]>
-			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->	
-		
-			<!-- respond.js -->
-		<!--[if lt IE 9]>
-		          <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
-		<![endif]-->	
+		<!-- Custom styles for this template -->
+		<link href="<?php bloginfo('template_url'); ?>/css/style.css" rel="stylesheet">
+
 	</head>
 	
-	<body <?php body_class(); ?>>
-				
-		<header role="banner">
-				
-			<div class="navbar navbar-default navbar-fixed-top">
+	<body id="page-top">
+
+	<div class="navbar-wrapper">
+		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header page-scroll">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+							aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">
+						<img src="<?php bloginfo('template_url'); ?>/img/logo.png" width="200px"></img>
+					</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a class="page-scroll" href="#page-top">Home</a></li>
+						<li><a class="page-scroll" href="#features">L&ouml;sungen</a></li>
+						<li><a class="page-scroll" href="#bintelligence">Business Intelligence</a></li>
+						<li><a class="page-scroll" href="#biweb">BI4web</a></li>
+						<li><a class="page-scroll" href="#contact">Kontakt</a></li>
+						<li><a class="page-scroll" href="#impressum">Impressum</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+	<div id="inSlider" class="carousel slide carousel-fade" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#inSlider" data-slide-to="0" class="active"></li>
+			<li data-target="#inSlider" data-slide-to="1"></li>
+			<li data-target="#inSlider" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
 				<div class="container">
-          
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
+					<div class="carousel-caption">
+						<h1>Business Intelligence</h1>
 
-						<a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+						<p>Mittelstands-L&ouml;sungen f&uuml;r
+							<br/>Ihre individuellen Anforderungen.</p>
 					</div>
+					<div class="carousel-image">
 
-					<div class="collapse navbar-collapse navbar-responsive-collapse">
-						<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
-
-						<?php //if(of_get_option('search_bar', '1')) {?>
-						<form class="navbar-form navbar-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-							<div class="form-group">
-								<input name="s" id="s" type="text" class="search-query form-control" autocomplete="off" placeholder="<?php _e('Search','wpbootstrap'); ?>">
-							</div>
-						</form>
-						<?php //} ?>
+						<?php
+						$post_thumbnail_id = get_post_thumbnail_id();
+						$featured_src = wp_get_attachment_image_src( $post_thumbnail_id, array(100,100));
+						if( $featured_src ) {
+							?>
+							<img src="<?php echo $featured_src[0]; ?>" width="<?php echo $featured_src[1]; ?>" height="<?php echo $featured_src[2]; ?>">
+						<?php } ?>
+						<img src="<?php bloginfo('template_url'); ?>/img/laptop.png" alt="laptop"/>
 					</div>
+				</div>
+				<!-- Set background for slide in css -->
+				<div class="header-back one"></div>
 
-				</div> <!-- end .container -->
-			</div> <!-- end .navbar -->
-		
-		</header> <!-- end header -->
-		
-		<div class="container">
+			</div>
+			<div class="item">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1>Internet of Things</h1>
+
+						<p>Intelligente Produkte.
+							<br/>Daten aus der ganzen Welt.</p>
+					</div>
+				</div>
+				<!-- Set background for slide in css -->
+				<div class="header-back two"></div>
+			</div>
+			<div class="item">
+				<div class="container">
+					<div class="carousel-caption blank">
+						<h1>Kompetente Beratung und
+							<br/> Softwareentwicklung.</h1>
+
+						<p>Individuelle Software und
+							<br/>intuitive Apps für komplexe Aufgaben.</p>
+					</div>
+				</div>
+				<!-- Set background for slide in css -->
+				<div class="header-back three"></div>
+			</div>
+		</div>
+		<a class="left carousel-control" href="#inSlider" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">zurück</span>
+		</a>
+		<a class="right carousel-control" href="#inSlider" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">vor</span>
+		</a>
+	</div>
+
